a9f95a36f4cec0980244009c66df51f8dfbfff5d
craig-glass <sydrther@gmail.com>
2021-10-23 21:01:14 +0100

    Changed host in database

--- config/settings.py
+++ config/settings.py
@@ -83,7 +83,7 @@ DATABASES = {
         'NAME': 'postgres',
         'USER': 'postgres',
         'PASSWORD': 'postgres',
-        'HOST': ['db', '172.18.0.2'],
+        'HOST': ['db', '0.0.0.0:5432'],
         'PORT': '5432'
     }
 }
