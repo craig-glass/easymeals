186d8856db8be9d8777da04d21222fa1b2e9668f
craig-glass <sydrther@gmail.com>
2021-10-24 14:12:03 +0100

    Added 0.0.0.0 to ALLOWED_HOSTS

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-e3ppa0!%m03zin#h!^wpbgk-*@hp%n)r)lyzh)tg_-lv(l)inx
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0']
 
 
 # Application definition
