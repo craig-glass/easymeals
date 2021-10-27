81c523d75c5f61934a1ef7885328f4b1c0c060a7
craig-glass <sydrther@gmail.com>
2021-10-27 15:33:54 +0100

    React build now working, django admin css not

--- config/settings.py
+++ config/settings.py
@@ -130,5 +130,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOWED_ORIGINS = [
     "http://0.0.0.0:3000",
     "http://127.0.0.1:3000",
-    "http://localhost:3000"
+    "http://localhost"
 ]
