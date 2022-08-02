80bd1904d9d899dc501b7324d77052cf6def8e4f
craig-glass <sydrther@gmail.com>
2022-08-02 14:35:32 +0100

    Changed db name in settings

--- config/settings.py
+++ config/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'config.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
+        'NAME': 'easymeals',
         'USER': 'postgres',
         'PASSWORD': 'postgres',
       #  'HOST': 'db',
