8abd52e6c765cf9424226db4037668b1c27cb766
craig-glass <sydrther@gmail.com>
2022-08-02 14:22:24 +0100

    added test

--- backend/tests.py
+++ backend/tests.py
@@ -1,1 +1,16 @@
 from django.test import TestCase
+from models import MealPack
+
+
+class MealPackTestCase(TestCase):
+    def setUp(self):
+        MealPack.objects.create(name="pack 9", calories=3000,
+                                price=90, description="pack containing 3000 calories")
+        MealPack.objects.create(name="pack 10", calories=4000,
+                                price= 100, description="pack containing 4000 calories")
+
+    def test_mealpacks_correct(self):
+        pack9 = MealPack.objects.get(name="pack 9")
+        pack10 = MealPack.objects.get(name="pack 10")
+        self.assertEqual(pack9.price, 90)
+        self.assertEqual(pack10.calories, 4000)
