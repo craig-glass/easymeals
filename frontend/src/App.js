da5ff9e3d6bc68f438cd9712f11d5033ce9960e0
craig-glass <sydrther@gmail.com>
2022-08-11 21:35:28 +0100

    Changed fetch in app.js to fetch server ip

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,7 +14,7 @@ function App() {
     const [mealpacks, setMealpacks] = useState([]);
 
     useEffect(() => {
-        fetch('http://localhost:8000/api/mealpacks/')
+        fetch('http://167.99.203.5:8000/api/mealpacks/')
             .then(response => response.json())
             .then(data => {
                 console.log('data is: ', data);
