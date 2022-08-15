0638770a1681c6de16e97f11cb40a5bb0e2e9ff0
craig-glass <sydrther@gmail.com>
2022-08-15 18:32:00 +0100

    Moved collectstatic to dockerfile

--- Dockerfile
+++ Dockerfile
@@ -3,5 +3,6 @@ ENV PYTHONUNBUFFERED=1
 WORKDIR /app
 COPY requirements.txt /app/
 RUN pip install -r requirements.txt
+RUN python manage.py collectstatic -link --noinput
 COPY . /app/
 
--- config/settings.py
+++ config/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'NAME': 'postgres',
         'USER': 'postgres',
         'PASSWORD': 'postgres',
-        'HOST': 'db',
+        'HOST': 'localhost',
         'PORT': '5432'
     }
 }
@@ -120,6 +120,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = "/static/"
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 
 # Default primary key field type
