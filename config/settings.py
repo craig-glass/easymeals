cba05604b0d01c9f5da335c5edb7cb9bb9cb19dd
craig-glass <sydrther@gmail.com>
2022-08-15 18:41:48 +0100

    Moved collectstatic to dockerfile

--- config/settings.py
+++ config/settings.py
@@ -120,9 +120,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = "/static/"
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'web/static'),
-)
 STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 
 # Default primary key field type
